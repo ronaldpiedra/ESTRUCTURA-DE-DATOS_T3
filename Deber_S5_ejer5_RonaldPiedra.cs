be833580dcd26b86a5acbd1ef8d5349d5c68225a
ronaldpiedra <ra.piedraz@uea.edu.ec>
2025-06-29 20:02:30 -0500

    Create Deber_S5_ejer5_RonaldPiedra.cs
    Deber Semana 5 ejercicio 5 ronald Piedra

---
+++ Deber_S5_ejer5_RonaldPiedra.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class NumerosInversos
+{
+    private List<int> numeros;
+
+    public NumerosInversos()
+    {
+        numeros = Enumerable.Range(1, 10).ToList();
+    }
+
+    public void MostrarEnOrdenInverso()
+    {
+        numeros.Reverse();
+        Console.WriteLine("📉 Números del 10 al 1:");
+        Console.WriteLine(string.Join(", ", numeros));
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        NumerosInversos inversor = new NumerosInversos();
+        inversor.MostrarEnOrdenInverso();
+    }
+}
